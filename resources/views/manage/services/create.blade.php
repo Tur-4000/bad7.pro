931a06a1098d2c068dee1a9ab1ea672db2895875
Turbanov <tur.4000@gmail.com>
2020-04-02 00:44:19 +0300

    small refactor

--- resources/views/manage/services/create.blade.php
+++ resources/views/manage/services/create.blade.php
@@ -31,26 +31,6 @@
 
 @section('scripts')
 
-    <script>
-        $(document).ready(function() {
-            $('.description').summernote({
-                lang: 'ru-RU',
-                toolbar: [
-                    ['style', ['style']],
-                    ['font', ['bold', 'underline', 'clear']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['table', ['table']],
-                    ['insert', ['link']],
-                    ['view', ['fullscreen', 'codeview', 'help']]
-                ],
-                styleTags: [
-                    'p',
-                    { title: 'Подзаголовок', tag: 'h4', className: 'card__subtitle', value: 'p' },
-                    'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
-                ],
-            });
-        });
-    </script>
+    @include('manage.includes.summernote_editor')
 
 @endsection
